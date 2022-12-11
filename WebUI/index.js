f2389103f87ebdeddeb155388b13b2245b0079a1
YFHD <92370642+YFHD-osu@users.noreply.github.com>
2022-12-11 15:06:31 +0800

    Update index.js

--- WebUI/index.js
+++ WebUI/index.js
@@ -1,30 +1,33 @@
-function safariHacks() {
-    let windowsVH = window.innerHeight / 100;
-    document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
-    window.addEventListener('resize', function() {
-        document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
-    });
-}
-
 function sendWakeUp(deviceID) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", `/wake?device=${deviceID}`, true); 
-    xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", `/wake?device=${deviceID}`, true); 
+  xhr.send();
 }
 
 function logout() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/wake?device=1", true); 
-    xhr.send();
-    window.open("/logout","_self");
+  window.open("/logout","_self");
 }
 
 function refresh() {
-    var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/refresh?r=0", true); 
+  xhr.timeout = 10000;
+  xhr.send();
+
+  xhr.onreadystatechange=()=>{
+    if(xhr.readyState != XMLHttpRequest.DONE || xhr.status != 200) {return;}
+    deviceStatus = xhr.response.split("");
+    for (let i=0; i < deviceStatus.length; i++) {
+      console.log(parseInt(deviceStatus[i]));
+      document.getElementById(`deviceStatus${i}`).innerHTML = 
+      (parseInt(deviceStatus[i]) == 1) ? "線上" : "離線";
+      document.getElementById(`deviceImg${i}`).src = 
+      (parseInt(deviceStatus[i]) == 1) ? "https://yfhd-osu.github.io/ESP32-WOW/WebUI/Icons/onlineDot.png" : "https://yfhd-osu.github.io/ESP32-WOW/WebUI/Icons\\offlineDot.png";
+    }
     return;
-    xhr.open("GET", "/wake?device=1", true); 
-    xhr.send();
-    window.open("/logout","_self");
+  }
 }
 
-safariHacks();
+window.onload = function() {
+  refresh();
+};
