c04bbd4e3aa469e24b56f15991f3510b9cc8d7df
YFHD <ethan941106@gmail.com>
2024-04-21 03:11:12 +0800

    Update config

--- lib/ping.h
+++ lib/ping.h
@@ -6,14 +6,13 @@
 
 class Pinger {
   public: 
-
   static unsigned int last;
-  static int pins[4];
 
   static void init() {
-    for(int i=0; i<4 ; i++) {
-      pinMode(pins[i], OUTPUT);
-      digitalWrite(pins[i], LOW);
+    const int length = sizeof(LED_PINS) / sizeof(short); 
+    for(int i=0; i<length ; i++) {
+      pinMode(LED_PINS[i], OUTPUT);
+      digitalWrite(LED_PINS[i], LOW);
     }
   }
 
@@ -23,12 +22,12 @@ class Pinger {
     
     for (int i=0 ; i<DEVICE_COUNT ; i++) {
       devices[i].status = ping_start(devices[i].ip, 1, 1, 32, 100);
-      digitalWrite(pins[i], devices[i].status);
+      if (i >= sizeof(LED_PINS) / sizeof(short)) continue;
+      digitalWrite(LED_PINS[i], devices[i].status);
     }
   }
 };
 
-int Pinger::pins[4] = {4, 0, 2, 15};
 unsigned int Pinger::last = millis();
 
 #endif
